08d710ec887854724c7a1b3bd799f786a2b4901c
ohsuyuki <[ohsu@liquidinc.asia]>
2018-06-05 18:50:20 +0900

    [add] project

--- testStackView/ViewController.swift
+++ testStackView/ViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = (indexPath.row % 3 == 0 ? "IndexerCell" : "PalyerCell")
+        return tableView.dequeueReusableCell(withIdentifier: identifier)!
+    }
+
+    // UITableViewDelegate
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.row % 3 == 0 ? 24.0 : 60.0
+    }
+
+    // UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,4 +38,3 @@ class ViewController: UIViewController {
 
 
 }
-
